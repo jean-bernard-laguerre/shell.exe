827f7308f32c59883f29ecf11ae66fc36fea0946
JB <jean-bernard.laguerre@laplateforme.io>
2022-10-11 16:24:58 +0200

    corrections Job 8/9

--- Job_8/get_logs.sh
+++ Job_8/get_logs.sh
@@ -1,3 +1,2 @@
-filename=number_connection-$(date +'%d/%m/%Y-%H:%M')
-last | wc -l > "$filename"
-tar -cvf /Backup/"$filename".tar "$filename"
+filename=number_connection-$(date +'%d-%m-%Y-%H:%M')
+last jb | grep jb | wc -l > "$filename" && tar -cvf Backup/"$filename".tar "$filename"
--- Job_9/accessrights.sh
+++ Job_9/accessrights.sh
@@ -1,8 +1,9 @@
 while IFS=, read -r  id Prenom Nom Mdp Role
 do
-   useradd -m -p "$Mdp" "$Prenom_$Nom"
-   if[ $Role == "Admin" ]
+   sudo useradd -m -p "$Mdp" "$Prenom-$Nom"
+   if [ "$Role" = "Admin" ]
    then
-      sudo adduser "$Prenom_$Nom"
+      sudo usermod -aG sudo "$Prenom-$Nom"
+      groups "$Prenom-$Nom"
    fi
 done < <(tail -n +2 $1)
